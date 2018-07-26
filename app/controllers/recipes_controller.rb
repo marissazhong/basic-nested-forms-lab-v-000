8daeec6f67f7c4284b8c667bedf50970300da35c
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 16:00:16 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,6 +17,10 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title, ingredients: [:name, :quantity])
+    params.require(:recipe).permit(:title, ingredients:
+      [:recipe_ingredients_attributes_0_name,
+        :recipe_ingredients_attributes_0_quantity],
+        :recipe_ingredients_attributes_1_name,
+        :recipe_ingredients_attributes_1_quantity])
   end
 end
