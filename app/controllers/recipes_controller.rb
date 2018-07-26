ff56839307cfbeb0d055adad1b73e1451d8875ee
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 16:09:38 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,16 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes:
+      [:id, :name, :quantity])
   end
 end
